087440a57300fb2a0b7c27d05f5ad9b1ea0c6d01
AndruhaMan <andreysut12@gmail.com>
2023-03-14 21:28:20 +0200

    Add Vector and Point classes

---
+++ Objects/Vector.js
@@ -0,0 +1,31 @@
+module.exports = class Vector {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    normalize() {
+        return new Vector(
+            this.x / Math.sqrt(this.x**2 + this.y**2 + this.z**2),
+            this.y / Math.sqrt(this.x**2 + this.y**2 + this.z**2),
+            this.z / Math.sqrt(this.x**2 + this.y**2 + this.z**2)
+        );
+    }
+
+    subtract(vector) {
+        return new Vector(
+            this.x - vector.x,
+            this.y - vector.y,
+            this.z - vector.z
+        );
+    }
+
+    add(vector) {
+        return new Vector(
+            this.x + vector.x,
+            this.y + vector.y,
+            this.z + vector.z
+    )
+    }
+}
