b4a632a5bf2e8aac425dc375bd273ad14916e10e
Bodanka <opalkobogdana12@gmail.com>
2023-03-21 16:14:59 +0200

    Create main module

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Help!!!");
+const Sphere = require("./Objects/Sphere");
+const Ray = require("./Objects/Ray");
+const Point = require("./Objects/Point");
+const Vector = require("./Objects/Vector");
+const Camera = require("./Objects/Camera");
+
+const width = 100;
+const height = 100;
+const fov = Math.PI / 2;
+const aspectRatio = width / height;
+const canvas = new Array(width * height);
+
+const sphere = new Sphere(new Point(50, 50, 4), 3);
+const camera = new Point(width / 2, height / 2, -20);
+
+
+
+function castRay(x, y) {
+    const px = ((2 * (x + 0.5)) / width - 1) * Math.tan(fov / 2) * aspectRatio;
+    const py = (1 - (2 * (y + 0.5)) / height) * Math.tan(fov / 2);
+    const rayDirection = new Vector(px, py, -1).normalize();
+    const ray = new Ray(camera, rayDirection);
+    return sphere.sphereIntersect(ray);
+}
+
+for (let y = height - 1; y >= 0; y--) {
+  for (let x = 0; x < width; x++) {
+      if (castRay(x, y)) {
+          process.stdout.write("# ");
+      } else {
+          process.stdout.write(". ");
+      }
+  }
+  process.stdout.write("\n");
+  }
+
