e8e27cfe7e668755bc52331f2e5e99b37ad8979c
Annnyan <novikovaannna@gmail.com>
2023-05-23 20:12:40 +0300

    Add converter

--- Objects/ImageWriter.js
+++ Objects/ImageWriter.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-class ImageOutput {
-    consoleLog(image) {
+class ImageWriter {
+    writeConsole(image) {
         for (let y = image.height - 1; y >= 0; y--) {
             for(let x = 0; x < image.width; x++) {
                 process.stdout.write(image.pixels[y][x].getConsoleColor() + " ");
@@ -10,18 +10,16 @@ class ImageOutput {
         }
     }
 
-    fileLog(image, filename) {
+    writePPM(image, filename) {
         let fileContent = `P3\n${image.width} ${image.height}\n255\n`;
         for (let y = image.height - 1; y >= 0; y--) {
             for (let x = 0; x < image.width; x++) {
                 fileContent += image.pixels[y][x].colorToString() + "\n";
             }
         }
-        fs.writeFile(`${filename}.ppm`, fileContent, (err) => {
-            if (err) throw err;
-            console.log('File created!');
-        });
+        fs.writeFileSync(`Output/${filename}.ppm`, fileContent);
+        console.log('File created!');
     }
 }
 
-module.exports = new ImageOutput();
+module.exports = new ImageWriter();
---
+++ converter.js
@@ -0,0 +1,38 @@
+const ImageWriter = require("./Objects/ImageWriter");
+const ImageReader = require("./Objects/ImageReader");
+
+const source = process.env.npm_config_source;
+const goalFormat = process.env.npm_config_goalFormat;
+
+const filename = source.split(".")[0];
+const inputFormat = source.split(".")[1];
+let image;
+try {
+    readFile(filename, inputFormat);
+    try {
+        writeFile(filename, goalFormat);
+    } catch (e) {
+        throw e;
+    }
+} catch (e) {
+    console.log(e.message);
+}
+
+function readFile(filename, inputFormat) {
+    if(inputFormat === "ppm") {
+        image = ImageReader.readPPM(filename);
+    } else {
+        throw new Error("We does not support this input format");
+    }
+}
+
+function writeFile(filename, goalFormat) {
+    if(goalFormat === "ppm") {
+        ImageWriter.writePPM(image, filename);
+    } else if (goalFormat === "console") {
+        ImageWriter.writeConsole(image);
+    } else {
+        throw new Error("We does not support this goal format");
+    }
+}
+
